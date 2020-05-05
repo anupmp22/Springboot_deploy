d08ec816c4845a58f26da3a12a4f32b1340cb3da
anupmp <5559365-anupmp22@users.noreply.gitlab.com>
2020-05-05 17:14:09 +0530

    first commit

---
+++ src/main/java/de/codecentric/springbootsample/RecordRepository.java
@@ -0,0 +1,8 @@
+package de.codecentric.springbootsample;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record, Long> {
+}
